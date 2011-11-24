0b2393ad1a2191ea8c16c02e269908cba2d393fc
Peter Ansell <p_ansell@yahoo.com>
2011-11-24 16:54:54 +1000

    add tests for front page, /admin/profiles, and three versions of /admin/configuration/CURRENT/*

---
+++ src/test/java/org/bio2rdf/test/Bio2RDFWebTest.java
@@ -0,0 +1,91 @@
+package org.bio2rdf.test;
+/**
+ * 
+ */
+
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.queryall.query.Settings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class Bio2RDFWebTest extends AbstractQueryAllWebTest
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Bio2RDFWebTest.class);
+
+    /**
+     * Tests that the /admin/profiles page was generated using the correct headers and that it wasn't interrupted
+     */
+    @Test
+    public void testAdminProfiles()
+    {
+        this.getWebTester().gotoPage("/admin/profiles");
+        
+        this.getWebTester().assertTextPresent("Number of queries =");
+        this.getWebTester().assertTextPresent("Number of providers =");
+        this.getWebTester().assertTextPresent("Number of rdf normalisation rules =");
+        this.getWebTester().assertTextPresent("Number of profiles =");
+        
+        this.getWebTester().assertTextPresent("The following list is authoritative across all of the currently enabled profiles");
+        
+        this.getWebTester().assertTextPresent("Included providers: (");
+        
+        this.getWebTester().assertTextPresent("Excluded providers: (");
+        
+        this.getWebTester().assertTextPresent("Included queries: (");
+        
+        this.getWebTester().assertTextPresent("Excluded queries: (");
+        
+        this.getWebTester().assertTextPresent("Included rdfrules: (");
+        
+        this.getWebTester().assertTextPresent("Excluded rdfrules: (");
+        
+        this.getWebTester().assertTextPresent("The next section details the profile by profile details, and does not necessarily match the actual effect if there is more than one profile enabled");
+    }
+
+    /**
+     * Tests that the /admin/configuration/CURRENT/n3 interface is working correctly
+     */
+    @Test
+    public void testCurrentApiVersionAdminConfigurationN3()
+    {
+        this.getWebTester().gotoPage("/admin/configuration/"+Settings.CONFIG_API_VERSION+"/n3");
+        
+    }
+    
+    /**
+     * Tests that the /admin/configuration/CURRENT/rdfxml interface is working correctly
+     */
+    @Test
+    public void testCurrentApiVersionAdminConfigurationRdfXml()
+    {
+        this.getWebTester().gotoPage("/admin/configuration/"+Settings.CONFIG_API_VERSION+"/rdfxml");
+    }
+
+    /**
+     * Tests that the /admin/configuration/CURRENT/json interface is working correctly
+     */
+    @Test
+    public void testCurrentApiVersionAdminConfigurationJson()
+    {
+        this.getWebTester().gotoPage("/admin/configuration/"+Settings.CONFIG_API_VERSION+"/json");
+    }
+
+    @Override
+    protected String getBaseUrl()
+    {
+        return "http://localhost:9090/bio2rdf-test/";
+    }
+
+    @Override
+    protected String getBeginAtPath()
+    {
+        return "/";
+    }
+    
+}
